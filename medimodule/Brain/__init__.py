d9f1364130667b8fcf38aebc55b81bc1a2994b31
Sungman-Cho <pccho9108@gmail.com>
2020-10-22 15:39:50 +0900

    update chest, brain

--- medimodule/Brain/__init__.py
+++ medimodule/Brain/__init__.py
@@ -1,3 +1,3 @@
-from .module import BrainSegmentation
+from .module import BlackbloodSegmentation
 from .module import MRA_BET  
 
--- medimodule/Brain/mra_bet/load_model.py
+++ medimodule/Brain/mra_bet/load_model.py
@@ -1,14 +1,10 @@
 import torch
 import os
 
-from .model.Unet import UNet
+from .models.Unet import UNet
 
 def build_MRA_BET(weight_path, gpu_num):
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_num)
-    device = torch.device(f'cuda:{0}' if torch.cuda.is_available() else 'cpu')
-    torch.cuda.set_device(device)
-    
-    model = UNet().to(device)
+    model = UNet().cuda()
     weight = torch.load(weight_path)#, map_location='cpu')
     model.load_state_dict(weight['net'])
     
