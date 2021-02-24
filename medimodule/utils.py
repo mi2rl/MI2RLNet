625b8b86e6565abefcb7b7a2ceb623f49eb14990
Sungchul Kim <sungchul7039@gmail.com>
2021-02-24 10:20:36 +0900

    Edit Checker

--- medimodule/utils.py
+++ medimodule/utils.py
@@ -4,24 +4,28 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import sys
 import logging
 import warnings
+from typing import List
 
 modules = {
-    'chest' : [
+    'Chest' : [
         'lrmark_detection',
         'viewpoint_classification',
         'enhance_classification'
     ],
-    'brain' : [
+    'Brain' : [
         'mribet',
         'mrabet',
         'blackblood_segmentation'
+    ],
+    'Abdomen': [
+        'liver_segmentation'
     ]
 }
 
 class Checker:
 
     @staticmethod
-    def check_input_type(_input, _t):
+    def check_input_type(_input: str, _t: List[str]):
         """
         Check a input image type.
 
@@ -35,10 +39,10 @@ class Checker:
             However, if there are some problems, it will makes a assertion error.
         """
         input_ext = _input.split('.')[-1]
-        assert input_ext == _t, 'expected {} file extension, but got {} file extension'.format(_t, input_ext)
+        assert input_ext in _t, 'expected {} file extension, but got {} file extension'.format(_t, input_ext)
 
     @staticmethod
-    def check_input_type_bool(_input, _t):
+    def check_input_type_bool(_input: str, _t: List[str]) -> bool:
         """
         Check a input image type and return bool.
 
@@ -51,7 +55,7 @@ class Checker:
             (bool) Whether the correct format or not
         """
         input_ext = _input.split('.')[-1]
-        if input_ext == _t:
+        if input_ext in _t:
             return True
         else:
             return False
@@ -72,43 +76,30 @@ class Checker:
         assert gpu_idx is not None, 'gpu_idx must be selected.'
         assert isinstance(gpu_idx, str)
         assert framework is not None, 'framework must be selected.'
-        assert framework in ['tf1', 'keras_tf1', 'tf2', 'pytorch'], \
-            'This module supports these frameworks, tf1, keras_tf1, tf2, and pytorch.'
+        assert framework in ['tf', 'pytorch'], \
+            'This module supports these frameworks, tf and pytorch.'
         
         os.environ["CUDA_VISIBLE_DEVICES"] = gpu_idx
         if gpu_idx == '-1':
             warnings.warn('Running time may be long in cpu mode.')
         else:
-            if 'tf' in framework:
+            if 'tf' == framework:
+                # only tensorflow 2.x
                 import tensorflow as tf
-                if framework == 'tf1':
-                    # tensorflow 1.x
-                    assert int(tf.__version__.split('.')[0]) < 2.0, \
-                        'If the environment consists of tf1, the argument of framework must contain \'tf1\'.'
-
-                    # How to allow gpu increase without returning session?
-                    raise NotImplementedError(
-                        'tf1 is not implemented yet. How to allow gpu increase without returning session?'
-                        'We welcome your any advices.')
-                elif framework == 'keras_tf1':
-                    # keras with tensorflow 1.x
-                    assert int(tf.__version__.split('.')[0]) < 2.0, \
-                        'If the environment consists of tf1, the argument of framework must contain \'tf1\'.'
-                    import keras
-                    config = tf.ConfigProto()
-                    config.gpu_options.allow_growth = True
-                    keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
-                elif framework == 'tf2':
-                    # tensorflow 2.x
-                    assert int(tf.__version__.split('.')[0]) >= 2.0
-                    gpus = tf.config.experimental.list_physical_devices('GPU')
-                    if gpus:
-                        try:
-                            for gpu in gpus:
-                                tf.config.experimental.set_memory_growth(gpu, True)
-                        except RuntimeError as e:
-                            # Memory growth must be set before GPUs have been initialized
-                            print(e)
+                assert int(tf.__version__.split('.')[0]) >= 2.0
+                gpus = tf.config.experimental.list_physical_devices('GPU')
+                if gpus:
+                    try:
+                        for gpu in gpus:
+                            tf.config.experimental.set_memory_growth(gpu, True)
+                        print(f"Complete to allocate gpu(s) at {gpu_idx} and set memory growth in TF2.")
+                    except RuntimeError as e:
+                        # Memory growth must be set before GPUs have been initialized
+                        print(e)
+            else:
+                # pytorch
+                print(f"Complete to allocate gpu(s) at {gpu_idx}.")
+                            
     @staticmethod
     def check_args(part, task):
         if part not in modules.keys():
